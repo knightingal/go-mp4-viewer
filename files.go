334d60a38ccc2056755e50027f4e1d7db4008cc1
knightingal <yujianming1985@hotmail.com>
2024-01-31 22:07:17 +0800

    sort dirNames by mod time

--- files.go
+++ files.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/fs"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -10,10 +11,22 @@ func scanBaseDir(baseDir string) []string {
 	dir := os.DirFS(baseDir)
 	dirEntityList, _ := fs.ReadDir(dir, ".")
 	fileNames := make([]string, 0)
+	dirFileEntiryList := make([]fs.DirEntry, 0)
+
 	for _, file := range dirEntityList {
 		if file.IsDir() {
-			fileNames = append(fileNames, file.Name())
+			dirFileEntiryList = append(dirFileEntiryList, file)
 		}
+	}
+
+	sort.Slice(dirFileEntiryList, func(i, j int) bool {
+		infoI, _ := dirFileEntiryList[i].Info()
+		infoJ, _ := dirFileEntiryList[j].Info()
+		return infoI.ModTime().Compare(infoJ.ModTime()) > 0
+	})
+
+	for _, file := range dirFileEntiryList {
+		fileNames = append(fileNames, file.Name())
 	}
 	return fileNames
 }
